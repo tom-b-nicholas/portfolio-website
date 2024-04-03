20eaae66340c53d2c075a0c177a455132a523179
Tom Nicholas <tomblyth90@gmail.com>
2024-04-03 15:06:05 +1100

    About page text update

--- app/routes/about/route.tsx
+++ app/routes/about/route.tsx
@@ -23,22 +23,22 @@ export default function About() {
         </div>
         <div className = 'about__text-container'>
           <p>
-            I am a junior software engineer with a background in biostatistics and public health research. I made the switch to engineering because I wanted a career where I had to constantly update my knowledge. After a while I realised the fast pased world of web development is where I belonged.  
+            I am a junior software engineer with a background in biostatistics and public health research. I made the switch to engineering because I wanted a career where I had to constantly update my knowledge. After a while I realised the fast paced world of web development is where I belonged.  
           </p>
         </div>
       </div>
       <div className = 'container-wrap part-b'>
         <p>
-          In 2016 I completed a Bachelor of Science majoring in Mathematics at the University of New South Wales. During my Bachelors degree I completed a unit called the theory of statistics where I studied probability and distribution theory. This introduced me to the mathematical foundations for statistics.
+          In 2016 I completed a Bachelor of Science majoring in Mathematics at the University of New South Wales. During my Bachelor’s degree I completed a unit called the theory of statistics where I studied probability and distribution theory. This introduced me to the mathematical foundations for statistics.
         </p>
         <p>
-          In 2017 I started studying a Master of Medical Sciences majoring in Public Health Sciences from the Karolinska Institute in Stockholm, Sweden. I learned solid research skills as well as advancing my biostatistical skills. I completed my Master's degree in 2019 with an original research project involving longitiudinal data analysis.
+          In 2017 I started studying a Master of Medical Sciences majoring in Public Health Sciences from the Karolinska Institute in Stockholm, Sweden. I learned solid research skills as well as advancing my biostatistical skills. I completed my Master's degree in 2019 with an original research project involving longitudinal data analysis.
         </p>
         <p>
-          In 2023 I decided to make the switch to software engineering and I started the Software Engineering Immersive course at General Assembly. I learned solid skills in Javascript, HTML, and CSS. I learned also gained experience using React.
+          In 2023 I decided to make the switch to software engineering and I started the Software Engineering Immersive course at General Assembly. I learned solid skills in JavaScript, HTML, and CSS. I learned also gained experience using React.
         </p>
         <p>
-          I am currently learning new skills and enjoy making projects with frameworks such as Remix and NEXT. Thanks for reading my story! Check out my projects and send me an email if you'd like to chat!
+          I am currently learning new skills and enjoy making projects with web frameworks such as Remix. Thanks for reading my story! Check out my projects and send me an email if you'd like to chat!
         </p>
       </div>
       <div className = 'section'></div>
